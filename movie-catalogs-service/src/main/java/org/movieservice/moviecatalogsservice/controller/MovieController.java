cbaa183ee3f8bb22cafa3c8c7ca4780d17e87b13
Supun Amarasinghe <58842675+supun-dev@users.noreply.github.com>
2020-08-15 22:23:01 +0800

    API call to the movie info service and get the movie details
    Pretty basic work - Lot of hardcoding. Usage of resttemplate.

--- movie-catalogs-service/src/main/java/org/movieservice/moviecatalogsservice/controller/MovieController.java
+++ movie-catalogs-service/src/main/java/org/movieservice/moviecatalogsservice/controller/MovieController.java
@@ -1,10 +1,14 @@
 package org.movieservice.moviecatalogsservice.controller;
 
 import org.movieservice.moviecatalogsservice.model.CatalogItem;
+import org.movieservice.moviecatalogsservice.model.Movie;
+import org.movieservice.moviecatalogsservice.model.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.*;
 
 @RestController
@@ -13,7 +17,21 @@ public class MovieController {
 
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getStatus(@PathVariable("userId") String userId) {
-		return Collections.singletonList(new CatalogItem(userId, "Save Humanity", 8));
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<Rating> listRating = Arrays.asList(new Rating("Train to Busan", 8), new Rating("Australia", 9));
+		List<CatalogItem> listCatalogItems = new ArrayList<>();
+
+		for (Rating rating : listRating) {
+			String smovie = restTemplate.getForObject("http://127.0.0.1:8082/movies/1102", String.class);
+			System.out.println("Movie : " + smovie);
+			Movie movie = restTemplate.getForObject("http://127.0.0.1:8082/movies/1102", Movie.class);
+			CatalogItem catelogItem = new CatalogItem(movie.getName(), "Save Humanity", rating.getRating());
+			listCatalogItems.add(catelogItem);
+		}
+
+		return listCatalogItems;
 	}
 
 }
--- movie-info-service/src/main/java/org/movieservice/movieinfoservice/controller/MovieInfoController.java
+++ movie-info-service/src/main/java/org/movieservice/movieinfoservice/controller/MovieInfoController.java
@@ -1,8 +1,5 @@
 package org.movieservice.movieinfoservice.controller;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.movieservice.movieinfoservice.model.Movie;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieInfoController {
 
 	@RequestMapping("/{movieId}")
-	public List<Movie> getMovieInfo(@PathVariable("movieId") String movieId)
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId)
 	{
-		return Collections.singletonList(new Movie("1102", "Guardians of the galaxy"));
+		return new Movie("1102", "Guardians of the galaxy");
 	}
 	
 }
